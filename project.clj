d79a6d07e2bd91b5c125b6a020274693f6d94c08
Ian Cohen <ianrcohen@gmail.com>
2013-09-19 23:29:08 -0400

    convert postgres table to datums and pprint 2 of them in main for now

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject postgres2datomic "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.8.4143"]
+                 [org.clojure/java.jdbc "0.3.0-alpha5"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]]
+
+  :main postgres2datomic.core)
---
+++ src/postgres2datomic/core.clj
@@ -0,0 +1,53 @@
+(ns postgres2datomic.core
+  (:require [clojure.java.jdbc :as jdbc]
+            [datomic.api :as d :refer [db q]]
+            [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clojure.pprint :refer [pprint]])
+  (:gen-class))
+  
+(defn reset-datomic [uri]
+  "Return a connection to a new database."
+  ;; This will fail if the DB doesn't exist. But who cares?
+  ;; See http://docs.datomic.com/clojure-api.html
+  (d/delete-database uri)
+  (d/create-database uri)
+  (d/connect uri))
+
+(defn keywordize [s]
+  "Convert a string to an idomatic keyword"
+  (-> (str/lower-case s)
+      (str/replace "_" "-")
+      (str/replace "." "-")
+      (keyword)))
+
+(defn typer [pg-type]
+  "Convert a postgres column type to a datomic datum type"
+  (cond
+   (= pg-type "character varying") "string"
+   (= pg-type "smallint") "long"
+   (= pg-type "bigint") "long"
+   (= pg-type "text") "string"))
+   
+(defn postgres-table-schema [db table]
+  "Query a postgres database for a table schema"
+  (jdbc/query db
+    ["select table_name, column_name, data_type from INFORMATION_SCHEMA.COLUMNS where table_name = ?" table]))
+
+(defn datumize [{:keys[table_name column_name data_type]}]
+  "Convert a postgres table column type to a datum schema"
+   {:db/id (d/tempid :db.part/db)
+    :db/ident (keyword (str table_name "/" column_name))
+    :db/valueType (keyword (str "db.type/" (typer data_type)))
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db})
+
+(defn main
+  "Main - used for testing for now..."
+  [table]
+  (let [config (edn/read-string (slurp "config.edn"))]
+    (reset-datomic (get-in config [:datomic :uri]))
+    (pprint (take 2 (map datumize (postgres-table-schema (:postgres config) table)))))
+  )
+  
+
